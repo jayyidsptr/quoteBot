ccddd93146e6d0bfa1e8aebc5171f60faf846f23
Jayyid <45623599+jayyidsptr@users.noreply.github.com>
2023-08-10 16:02:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const bot = new TelegramBot(botToken, { polling: true });
 
 const queries = ['bucin', 'galau', 'kehidupan', 'random'];
 
-cron.schedule('55 14 * * *', async () => {
+cron.schedule('04 16 * * *', async () => {
     const channelId = '@katanyanala';
 
     for (let i = 0; i < 5; i++) {
